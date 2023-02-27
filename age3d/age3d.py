08f1b2fdff16792b23da03b7b6f8e39f7818c291
Abhishek Chaudhary <ac5003@columbia.edu>
2023-02-26 20:15:53 -0500

    Moved age3d.py to /age3d

--- age3d/age3d.py
+++ age3d/age3d.py
@@ -98,26 +98,26 @@ def main():
     vertices = find_minimum(mesh, 1000)
     min_point_cloud = make_point_cloud(vertices, [255,0,0])
 
-    # threshold = 80000
+    threshold = 80000
 
-    # vertices = find_all_below(mesh, threshold)
-    # below_minimum_cloud = make_point_cloud(vertices, [0,0,255])
+    vertices = find_all_below(mesh, threshold)
+    below_minimum_cloud = make_point_cloud(vertices, [0,0,255])
 
-    # vertices = find_all_above(mesh, threshold)
-    # above_minimum_cloud = make_point_cloud(vertices, [0,255,0])
+    vertices = find_all_above(mesh, threshold)
+    above_minimum_cloud = make_point_cloud(vertices, [0,255,0])
 
-    # visualize([mesh, min_point_cloud, below_minimum_cloud, above_minimum_cloud])
+    visualize([mesh, min_point_cloud, below_minimum_cloud, above_minimum_cloud])
 
-    lower_threshold = 80000
-    higher_threshold = 80000 * 2
+    # lower_threshold = 80000
+    # higher_threshold = 80000 * 2
 
-    vertices = find_all_between(mesh, lower_threshold, higher_threshold)
-    between_minimum_cloud = make_point_cloud(vertices, [0,0,255])
+    # vertices = find_all_between(mesh, lower_threshold, higher_threshold)
+    # between_minimum_cloud = make_point_cloud(vertices, [0,0,255])
 
-    visualize([mesh, min_point_cloud, between_minimum_cloud])
+    # visualize([mesh, min_point_cloud, between_minimum_cloud])
 
 
-    vertices = get_neighbors(mesh, 0)
+    vertices = find_neighbors(mesh, 0)
 
 
 
